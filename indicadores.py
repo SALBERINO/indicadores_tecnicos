ce41c2b082de6b52d1637ea9c36a6d07d85d6af1
SALBERINO <89280712+SALBERINO@users.noreply.github.com>
2021-09-04 21:46:32 -0300

    Update indicadores.py
    Agrego indicador basico de gaps basado en tendencia de 2 días atras solamente

--- indicadores.py
+++ indicadores.py
@@ -5,3 +5,12 @@ def devolver_con_percentil(df):
   data["rank_variacion"] = data["variacion"].rank()
   data["rank_variacion_pct"] = data["variacion"].rank(pct= True)
   return data
+
+def indicador_gap_basico(data):
+  import numpy as np
+  df = data.copy()
+  df_menosuno = data.copy().shift(1)
+  df_menosdos = data.copy().shift(2)
+  df_menosuno['tendencia'] = np.where(df_menosuno['Close'] > df_menosdos['Close'],'alsista','bajista')
+  df['gap'] = np.where((df_menosuno['tendencia'] == 'alsista') & (df['Open'] > df_menosuno['High']) & (df['Close'] > df['Open']), 'gap alsista', np.where((df_menosuno['tendencia'] == 'bajista)') & (df['Open'] < df_menosuno['Low']) & (df['Close'] < df['Open']),'gap bajista', 'sin gap'))
+  return df
